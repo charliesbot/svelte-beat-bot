9879245b63d6a60679feda52888646c8ec9c29dc
Charlie L <charliesbot@gmail.com>
2021-08-04 22:28:31 -0700

    base ui

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,98 @@
 <script lang="ts">
-  export let name: string;
+  import Song from "./components/Song.svelte";
+  import BetterScroll from "@better-scroll/core";
+  import { distanceBetweenPoints, getCenter } from "./utils/layout";
+  import { songs } from "./songs";
+
+  let wrapper: HTMLDivElement;
+  let currentSize = 200;
+
+  $: if (wrapper) {
+    console.log("papita");
+    let bscroll: BetterScroll = new BetterScroll(wrapper, {
+      freeScroll: true,
+      probeType: 3,
+      /* scrollbar: false, */
+      /* scrollX: true, */
+      /* scrollY: true, */
+    });
+    bscroll.scroller.scrollToElement(
+      songs[Math.floor(songs.length / 2)].node,
+      1000,
+      currentSize / 2,
+      currentSize / 2
+    );
+
+    bscroll.on("scroll", () => {
+      const distances = songs.map((s) => {
+        const rect = s.node.getBoundingClientRect();
+        const coords = {
+          x: rect.left + rect.width / 2,
+          y: rect.top + rect.height / 2,
+        };
+
+        const origin = getCenter(document.body);
+
+        const calculatedDistance = distanceBetweenPoints(origin, coords);
+
+        const delta = 2 - calculatedDistance / (currentSize * 1.65);
+
+        const limitedDelta = Math.max(delta, 1);
+
+        const zIndex = Math.ceil(limitedDelta * 100);
+
+        return { delta: limitedDelta, zIndex };
+      });
+
+      songs.forEach((song, index) => {
+        const s = song.node;
+        const { delta, zIndex } = distances[index];
+        s.style.transform = `scale3d(${delta}, ${delta}, ${delta})`;
+        delta > 1.5
+          ? s.classList.add("show-overlay")
+          : s.classList.remove("show-overlay");
+        s.style.zIndex = zIndex.toString();
+      });
+    });
+  }
 </script>
 
-<main>
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-    how to build Svelte apps.
-  </p>
+<main bind:this={wrapper}>
+  <aside
+    class="content"
+    style="
+      --width:{currentSize * 7}px;
+      --height:{Math.ceil(songs.length / 7) * currentSize}"
+  >
+    {#each songs as song}
+      <Song size={currentSize} {song} bind:node={song.node} />
+    {/each}
+  </aside>
 </main>
 
-<style>
+<style lang="scss">
   main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
+    width: 3.125rem;
+    height: 3.125rem;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-left: -1.5625rem;
+    margin-top: -1.5625rem;
   }
 
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
+  aside {
+    display: grid;
+    grid-template-columns: repeat(7, 200px);
+    grid-template-rows: repeat(7, 200px);
+    position: relative;
+    color: white;
+    width: var(--width);
+    height: var(--height);
+    transform-style: preserve-3d;
+    cursor: grab;
+    &:active {
+      cursor: grabbing;
     }
   }
 </style>
---
+++ src/components/Song.svelte
@@ -0,0 +1,97 @@
+<script lang="ts">
+  import type { SongType } from "../types/types";
+  export let node: HTMLDivElement;
+  export let size: number;
+  export let song: SongType;
+
+  const { album, previewUrl } = song;
+  const coverArt = album.images[1].url;
+</script>
+
+<div
+  class="song"
+  style="--size:{size}px; --coverArt:url({coverArt})"
+  bind:this={node}
+>
+  <div class="overlay">
+    <section class="info">
+      <strong class="songTitle">{song.name}</strong>
+      <span class="songArtist">{song.artists[0].name}</span>
+    </section>
+  </div>
+</div>
+
+<style lang="scss">
+  .song {
+    display: flex;
+    top: 0;
+    left: 0;
+    width: var(--size);
+    height: var(--size);
+    position: relative;
+    background-size: cover;
+    border-radius: 0.25rem;
+    overflow: hidden;
+    will-change: transform;
+    border: none;
+    box-shadow: 0px 0px 5px 4px rgba(0, 0, 0, 0.3);
+    background-image: var(--coverArt);
+    &.show-overlay .overlay {
+      opacity: 1;
+    }
+  }
+
+  button {
+    flex: 1;
+    box-shadow: none;
+    border: none;
+    background-color: rgba(255, 255, 255, 0);
+    cursor: pointer;
+    svg {
+      pointer-events: none;
+    }
+  }
+
+  section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    flex: 1;
+    max-width: 100%;
+  }
+
+  .overlay {
+    display: flex;
+    align-content: center;
+    flex-direction: column;
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    opacity: 0;
+    transition: 100ms ease opacity;
+    background-color: rgba(0, 0, 0, 0.6);
+    color: white;
+    padding: 0.5rem;
+    font-size: 0.75rem;
+    span {
+      margin-top: 0.125rem;
+      font-size: 0.5rem;
+    }
+  }
+
+  ul {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 3rem;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+  }
+</style>
