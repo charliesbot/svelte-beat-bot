ec21d2364e3147ea4defb10cf1c9ff4e65e87ebb
Charlie L <charliesbot@gmail.com>
2021-08-15 22:24:03 -0700

    responsive

--- src/routes/graphqlClient.ts
+++ src/routes/graphqlClient.ts
@@ -3,6 +3,6 @@ import { GraphQLClient } from "graphql-request";
 const dev = "http://localhost:3000/graphql";
 const prod = "https://svelte-beat-bot.vercel.app/graphql";
 
-export const graphqlClient = new GraphQLClient(dev, {
+export const graphqlClient = new GraphQLClient(prod, {
   headers: {},
 });
