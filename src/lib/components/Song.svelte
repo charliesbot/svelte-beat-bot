f7f29f60f24adc571ff18cf2e045b9a2f7e31578
Charlie L <charliesbot@gmail.com>
2021-08-16 21:22:44 -0700

    change icon

--- src/lib/components/Song.svelte
+++ src/lib/components/Song.svelte
@@ -29,23 +29,7 @@
     </section>
     {#if previewUrl !== undefined}
       <section class="actions">
-        <button on:click={toggleSong}>
-          <svg
-            aria-hidden="true"
-            focusable="false"
-            data-prefix="fas"
-            data-icon="play"
-            role="img"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 448 512"
-            class="svg-inline--fa fa-play fa-w-14 fa-2x"
-            ><path
-              fill="currentColor"
-              d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z"
-              class="play-icon"
-            /></svg
-          >
-        </button>
+        <button on:click={toggleSong}> Play </button>
       </section>
     {/if}
   </div>
@@ -79,6 +63,8 @@
     cursor: pointer;
     height: 50px;
     width: 50px;
+    color: white;
+    font-weight: bold;
     svg {
       width: 20px;
       color: white;
